7ba3fe73c7eeaa351a98a947eee38872b9b91754
Alexander Lukyanov <cybergomel@gmail.com>
2021-04-20 02:25:23 -0400

    Changed 7.4 cli and fpm containers

--- php/7.4-fpm/docker-entrypoint.sh
+++ php/7.4-fpm/docker-entrypoint.sh
@@ -31,11 +31,6 @@ fi
 mkdir -p $MAGENTO_ROOT
 chown www-data:www-data $MAGENTO_ROOT
 
-# Configure Sendmail if required
-if [ "$ENABLE_SENDMAIL" == "true" ]; then
-    /etc/init.d/sendmail start
-fi
-
 # Substitute in php.ini values
 [ ! -z "${PHP_MEMORY_LIMIT}" ] && sed -i "s/!PHP_MEMORY_LIMIT!/${PHP_MEMORY_LIMIT}/" /usr/local/etc/php/conf.d/zz-magento.ini
 [ ! -z "${UPLOAD_MAX_FILESIZE}" ] && sed -i "s/!UPLOAD_MAX_FILESIZE!/${UPLOAD_MAX_FILESIZE}/" /usr/local/etc/php/conf.d/zz-magento.ini
